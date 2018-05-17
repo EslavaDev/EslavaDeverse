fc03e7b88565e9ce50cc0e355487a67e2a8df021
@wihermes <wihermes1@gmail.com>
2018-05-17 16:16:42 -0500

    update 2

--- EslavaDeverse-db/index.js
+++ EslavaDeverse-db/index.js
@@ -1,6 +1,27 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
 module.exports = async (config) => {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  // --------RELACIONES CON SEQUALIZE------------//
+
+  AgentModel.hasMany(MetricModel) // el 1 tiene muchos del 2
+  MetricModel.belongsTo(AgentModel) // el 1 pertenece a 2
+
+  // --------FIN DE RELACION SEQUALIZE-----------//
+
+  // para validad que la base de datos esta bien configurada
+  await sequelize.authenticate()
+
+  // configuraciond e la base de datos
+  sequelize.sync()
+
   const Agent = {}
   const Metric = {}
 
